27362d23a5c6ebaffe7589bca8e4a1c85de42c92
Seth Duncan <sethduncan@gmail.com>
2023-12-18 00:49:45 -0600

    more code

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print('Hello World')
+print('\n')
+print('*****')
+print('*   *')
+print('*   *')
+print('*   *')
+print('*****')
+print('\n')
+print('These are on the', end=' ')
+print('same line.')
+
+year = 2023
+print('Current year:', year)
+
+age = 39
+print('At the time of writing, I am', age, 'years old.')
---
+++ input.py
@@ -0,0 +1,5 @@
+print('You are standing in a field.\nYou have a wide river to the west.\nThere is a cliff to the east.\nThere is a dark forest to the south.\nA beautiful wind-swept plain is to the north.\nWhich direction do you want to go?')
+print('=>', end=' ')
+direction = input()
+
+print('You travel', direction, ' and are immediately killed by a wombat.')
