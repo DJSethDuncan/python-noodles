042b4d6eb10c8dcafb8e0dfd8545b2d6c8da8a7a
Seth Duncan <sethduncan@gmail.com>
2023-12-20 23:14:42 -0600

    update classes, add recursion

---
+++ algorithms/recursion.py
@@ -0,0 +1,15 @@
+# Recursion is irritating but doing the fibonacci thing is a good example
+
+def fibonacci(v1, v2, run_cnt):
+  print('% s + % s = % s' % (v1, v2, (v1 + v2)))
+
+  if run_cnt <= 1:                    # base case -- we ran for the user-specified count
+    pass                              # do nothing
+  else:                               # recursive case
+    fibonacci(v2, v1+v2, run_cnt-1)   # fib
+
+print ('Fibonacci, yo')
+
+run_for = int(input('How many steps you want, bro? '))
+
+fibonacci(0, 1, run_for)
